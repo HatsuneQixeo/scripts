cca8ef60be899265dd940893050f4a6a0cb3c823
Hatsune Qixeo <qiyong831@gmail.com>
2023-02-19 19:08:15 +0800

    Bug fixes

--- 42header.sh
+++ 42header.sh
@@ -1,7 +1,8 @@
 function ft_fileisc()
 {
 	# Extract the suffix, saving every characters beyond the last '.' letter
-	suffix=${1##*.}
+	local	suffix=${1##*.}
+
 	return $([ "$suffix" == 'c' ] || [ "$suffix" == 'h' ])
 }
 
@@ -13,13 +14,13 @@ function header_echo()
 # Open the file in Vim and apply the header
 function apply_header()
 {
-	file="$1"
+	local	file="$1"
 	# Deadly bug with -e:
 	#	It can freeze the whole script if the given file does not exist?
 	#	or is it nonexist directory?
 	if ! [ -e "$file" ]
 	then
-		header_echo ": $file: No such directory or file (in apply_header)"
+		header_echo "$file: No such directory or file (in apply_header)"
 		return 1
 	fi
 	vim -e "$file" -c "Stdheader" -c "wq" > /dev/null
@@ -29,9 +30,9 @@ function apply_header()
 function apply_dir_headers()
 {
 	# Just to keep the display consistent
-	dir="${1//\//}/"
+	local	dir="${1//\//}"
 	# Get the list of files in the specified directory
-	arr_files="$(find "$dir" -name "*.c") $(find "$dir" -name "*.h")"
+	local	arr_files="$(find "$dir" -name "*.c") $(find "$dir" -name "*.h")"
 	# Return if nothing is found
 	if [ -z "$arr_files" ]
 	then
@@ -39,7 +40,7 @@ function apply_dir_headers()
 	fi
 	# Pattern substitution, ex: mikudir/miku.c mikudir/miku.h -> miku.c miku.h
 	arr_files="${arr_files//$dir\//}"
-	header_echo "$dir"
+	header_echo "$dir/"
 	for file in $arr_files
 	do
 		apply_header "$dir/$file" && echo "	$file"
@@ -57,7 +58,7 @@ fi
 
 for path in $args
 do
-	if ! [-e "$path" ]
+	if ! [ -e "$path" ]
 	then
 		header_echo "$path: No such directory or file"
 	elif [ -d "$path" ]
